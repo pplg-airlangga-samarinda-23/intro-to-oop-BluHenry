d9bccb91326cbfa961f90f59875a6e24b2632d67
tes <geogeryanemory@gmail.com>
2024-10-02 15:25:11 +0800

    menyelesaikan 3 soal public function

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,9 +2,18 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
+    public $panjang;
+    public $lebar;
+    public $Luas;
+    public $keliling;
 
 
     // deklarasikan metode konstruktor
+    public function __construct($panjang, $lebar)
+    {
+        $this -> panjang = $panjang;
+        $this -> lebar = $lebar;
+    }
 
 
     // deklarasikan metode mutator dan asesor
@@ -13,17 +22,18 @@ class PersegiPanjang
 
     public function hitungLuas()
     {
-        // lengkapi kode untuk menghitung luas persegi panjang
-
+        return $this -> panjang * $this -> lebar;
     }
 
     public function hitungKeliling()
-    {
-        // lengkapi dengan kode untuk menghitung keliling persegi panjang
-
+    {  
+        return 2*($this -> panjang + $this -> lebar);
     }
 }
 
 $contoh = new PersegiPanjang(12, 20);
 echo "Luas Persegi Panjang: " . $contoh->hitungLuas() . "<br>";
 echo "Keliling Persegi Panjang: " . $contoh->hitungKeliling() . "<br>";
+
+
+?>
--- soal-3/Mobil.php
+++ soal-3/Mobil.php
@@ -1,15 +1,50 @@
 <?php
 class Mobil {
     // lengkapi dengan atribut dan metode sesuai pada buku
+    public $merek;
+    public $warna;
+    public $kecepatan;
+
+    public function __construct($merek, $warna, $kecepatan)
+    {
+        $this -> merek = $merek;
+        $this -> warna =$warna;
+        $this -> kecepatan = $kecepatan;
+    }
+
 
     // contoh metode melaju()
     public function melaju() {
-        echo "Mobil sedang melaju.";
+        echo "Mobil hazure sedang melaju dengan kecepatan $this->kecepatan <br>";
+    }
+
+    public function berhenti(){
+        echo "mobil hazure mendadak berenti karena ada mobil warna $this->warna didepan <br>";
+    }
+    
+    public function perkenalan() {
+        echo "oh ya merek mobil yg sekarang dikendarai oleh hazure adalah $this->merek <br>";
+        echo "jadi hazure sekarang menuju taman untuk bertemu dengan sirkel randomiz <br>";
+    }
+
+    public function klakson() {
+        echo "hazure ngeklakson motor yg menghalangi jalannya <br>";
     }
     
 }
+
+
+$hazure = new mobil("randomizer", "Kuning", 100);
+echo $hazure -> melaju();
+echo $hazure -> berhenti();
+echo $hazure -> perkenalan();
+echo $hazure -> klakson();
+
 
 // buat sebuah objek mobil dan lengkapi atributnya
 
 
 // panggil keempat metode yang terdapat pada kelas Mobil
+
+
+?>
